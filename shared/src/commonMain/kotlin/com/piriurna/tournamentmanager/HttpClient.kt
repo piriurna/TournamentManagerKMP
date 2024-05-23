34a766cdcc53b286baf70d27bd82942502e30578
Franco Zalamena <francozalamena@gmail.com>
2024-05-23 22:33:02 +0100

    HttpClient

---
+++ shared/src/commonMain/kotlin/com/piriurna/tournamentmanager/HttpClient.kt
@@ -0,0 +1,7 @@
+package com.piriurna.tournamentmanager
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
+
+expect val client: HttpClient
